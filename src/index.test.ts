6e66bc251714754613edf2cbf9643aa6343aa7d3
Lawrence Zeng <lawrence.zeng@gmail.com>
2019-12-25 10:44:15 -0500

    fix test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,7 @@
 import { useAutoProgressEffect } from '.';
 
 test('initialize hook', () => {
-    const [value, setStart] = useAutoProgressEffect(true);
+    const [value, setStart] = useAutoProgressEffect(false);
     expect(value).toBe(0);
-    expect(setStart).toBeDefined;
+    expect(setStart).toBeDefined();
   });
