2393e5db5d1149c7c7c078e35b1f66d0ef6e8f13
Lawrence Zeng <lawrence.zeng@gmail.com>
2019-12-25 17:36:27 -0500

    more readable

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,28 +1,32 @@
-import { renderHook, act } from '@testing-library/react-hooks'
+import { renderHook, act, HookResult } from '@testing-library/react-hooks'
 import { useAutoProgressEffect } from '.';
 
 test('initialize hook returns [number, function]', () => {
-  const { result } = renderHook(() => useAutoProgressEffect(false)) ;
+  const { result } = renderHook(() => useAutoProgressEffect(false));
 
   // return [number, function]
   expect(result.current.length).toBe(2);
 
-  expect(result.current[0]).toBe(0);
-  expect(result.current[1]).toBeDefined();
+  const [value, setStart] = result.current;
+  expect(value).toBe(0);
+  expect(setStart).toBeDefined();
 });
 
 test('set start to true to false should return change value to 100', () => {
-  const { result } = renderHook(() => useAutoProgressEffect(true)) ;
+  const { result } = renderHook(() => useAutoProgressEffect(true));
 
-  // return [number, function]
-  expect(result.current.length).toBe(2);
-
-  expect(result.current[0]).toBe(0);
-  expect(result.current[1]).toBeDefined();
+  {
+    const [value, setStart] = result.current;
+    expect(value).toBe(0);
+    expect(setStart).toBeDefined();
+  }
 
   act(() => {
     const setStart = result.current[1];
     setStart(false);
   })
-  expect(result.current[0]).toBe(100);
+  {
+    const [value] = result.current;
+    expect(value).toBe(100);
+  }
 });
