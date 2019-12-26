efb4c85258ba28bf56bbc352d7ee81913225d6af
Lawrence Zeng <lawrence.zeng@gmail.com>
2019-12-25 22:45:09 -0500

    progressing

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,4 +1,4 @@
-import { renderHook, act, HookResult } from '@testing-library/react-hooks'
+import { renderHook, act } from '@testing-library/react-hooks'
 import { useAutoProgressEffect } from '.';
 
 test('initialize hook returns [number, function]', () => {
@@ -29,4 +29,53 @@ test('change start from true to false should return change value to 100', () => 
     const [value] = result.current;
     expect(value).toBe(100);
   }
+});
+
+test('set start to true to start progress value from 0, 25, 50, 75 to 90, then set start to false to stop and get back 100', () => {
+  const { result, waitForNextUpdate, wait } = renderHook(() => useAutoProgressEffect(false));
+
+  {
+    const [value, setStart] = result.current;
+    expect(value).toBe(0);
+    expect(setStart).toBeDefined();
+  }
+
+  act(() => {
+    const setStart = result.current[1];
+    setStart(true);
+  })
+
+  waitForNextUpdate().then(() => {
+    const [value] = result.current;
+    expect(value).toBe(25);
+  });
+
+  waitForNextUpdate().then(() => {
+    const [value] = result.current;
+    expect(value).toBe(50);
+  });
+
+  waitForNextUpdate().then(() => {
+    const [value] = result.current;
+    expect(value).toBe(75);
+  });
+
+  waitForNextUpdate().then(() => {
+    const [value] = result.current;
+    expect(value).toBe(90);
+  });
+
+  wait(() => (result.current[0] === 90), { timeout: 2000 }).then(() => {
+    // stop progress
+    act(() => {
+      const setStart = result.current[1];
+      setStart(false);
+    })
+    waitForNextUpdate().then(() => {
+      const [value] = result.current;
+      expect(value).toBe(100);
+    });
+  })
+
+
 });
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,46 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-export const useAutoProgressEffect = (start: boolean) : [number, (b: boolean) =>void] => {
-    const [value, setValue] = useState(0);
+const PROGRESS_VALUES = [0, 25, 75, 90];
+const INITIAL_VALUE = PROGRESS_VALUES[0];
+const LAST_VALUE = PROGRESS_VALUES[PROGRESS_VALUES.length - 1];
+const FINAL_VALUE = 100;
 
-    const setStart = (b: boolean) => {if (!b) setValue(100);}
+function findNext(n: number) {
+    const nextVal = PROGRESS_VALUES.find(v => v > n);
+    return (nextVal ? nextVal : LAST_VALUE);
+}
+
+const useValueState = (): [number, () => void, () => void] => {
+    const [value, setValue] = useState(INITIAL_VALUE);
+
+    const nextValue = () => (setValue(findNext(value)));
+    const finalValue = () => (setValue(FINAL_VALUE));
+    return [value, nextValue, finalValue];
+}
+
+export const useAutoProgressEffect = (start: boolean): [number, (start: boolean) => void] => {
+    const [value, nextValue, finalValue] = useValueState();
+    const [startProgress, setStartProgress] = useState(start);
+
+    const setStart = (s: boolean) => { 
+        if (s !== startProgress) {
+            setStartProgress(s);
+            // switching off
+            if (!s) finalValue(); 
+        }
+    }
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            if (startProgress) {
+                nextValue();
+            }
+        }, 200);
+
+        return function cleanUp() {
+            clearTimeout(timeout);
+        }
+    });
     
     return [value, setStart];
 }
